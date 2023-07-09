358bea6665135bb61cd0ab2e9596179fc5592217
Priyavrat Kumar <122096583+Priyavrat001@users.noreply.github.com>
2023-07-09 09:45:05 +0530

    added card and navbar

---
+++ src/components/Newsitems.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Newsitems extends Component {
+    render() {
+        let {title, description, imageUrl} = this.props;
+        return (
+            <div>
+                <div className="card my-4" style={{width: "18rem"}}>
+                    <img src={imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a href="#" className="btn btn-sm btn-primary">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
